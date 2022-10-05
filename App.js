8f6ce9a3f1bd3e1d2a9c2eeecab25cff0a41fb0f
shengchen1 <464463779@qq.com>
2022-10-05 13:51:23 -0700

    Added two bottom buttons at home screen and a black page for Explore

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-
 export default function App() {
-  const buttonClickedHandler = () => {
-    console.log('You have been clicked a button!');
-  };
+  return (<NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" 
+      component={HomeScreen} 
+      />
+      <Stack.Screen name="Explore" component={ExploreScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>);
+}
 
+const Stack = createNativeStackNavigator();
+
+const buttonClickedHandler = () => {
+  console.log('You have been clicked a button!');
+};
+
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.screen}>
 
@@ -23,10 +37,29 @@ export default function App() {
           <Text style={styles.buttonText}>Check In</Text>
         </TouchableOpacity>
       </View>
-
-      <View style={styles.bottomRow}>
+      <View style={{ padding: 10, flex: 1 }}>
+        <View style = {styles.bottomRow}>
+          <TouchableOpacity 
+          style = {styles.bottomButton}
+          onPress={() => navigation.navigate("Explore")}>
+            <Text style = {styles.bottomButtonLabel}>Profile</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style = {styles.bottomButton}
+          onPress={() => navigation.navigate("Explore")}> 
+          {/* Navigate page should be updated later */}
+            <Text style = {styles.bottomButtonLabel}>Explore</Text>
+          </TouchableOpacity>
+        </View>
       </View>
+    </View>
+  );
+}
 
+
+function ExploreScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Explore Screen</Text>
     </View>
   );
 }
@@ -66,5 +99,30 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 50,
     fontWeight: 'bold',
+  },
+  bottomButtonText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  bottomButton: {
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    borderRadius: 4,
+    backgroundColor: "gray",
+    alignSelf: "flex-start",
+    marginHorizontal: "1%",
+    marginBottom: 6,
+    minWidth: "48%",
+    textAlign: "center",
+  },
+  bottomRow: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  bottomButtonLabel: {
+    fontSize: 12,
+    fontWeight: "500",
+    color: "black",
+    textAlign: "center"
   }
 });
