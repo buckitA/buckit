362b8d56b28702258ac2cbfd9999d0e03bcac77a
Joe <gramstad.j@northeastern.edu>
2022-11-13 20:48:21 -0600

    inline styling removed

--- components/Screens/Profile.jsx
+++ components/Screens/Profile.jsx
@@ -1,11 +1,19 @@
-import {Text, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 import ProfileHeader from "../ProfileHeader";
 
 export default function ProfileScreen() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.profileContainer}>
             <Text>Profile</Text>
             <ProfileHeader></ProfileHeader>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    profileContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
